0cbc9e88f0d1eb63e7c6ccc04d70d2248a53ced2
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-04 00:29:43 -0500

    first commit puppy pals

---
+++ puppy-pals/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+//What do you observe?
+//Can you describe what is happening in this file?
+//Have you done this before?
