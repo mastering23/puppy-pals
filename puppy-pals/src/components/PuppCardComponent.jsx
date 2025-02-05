bb4d1c25d17ea74d7b7f6f70920b227bfec2bd49
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-05 03:43:44 -0500

    adding components and | fixing the logic

---
+++ puppy-pals/src/components/PuppCardComponent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const PuppyCardComponent = ({ puppy }) => {
+  return (
+    <div>
+      <h2>{puppy.name}</h2>
+      <p>Email: {puppy.email}</p>
+      <p>Age: {puppy.age}</p>
+      <p>Owner ID: {puppy.ownerId}</p>
+      <p>Tricks</p>
+      <p>----------------</p>
+      <div>
+      { puppy.tricks.length > 0 ? (<ul>
+        { puppy.tricks.map((trick) => (
+        <li key={trick.id}>
+          {trick.title}
+        </li>
+      ))}</ul>) : "No tricks" }
+      </div>
+      
+
+    </div>
+  );
+};
+
+export default PuppyCardComponent;
--- puppy-pals/src/main.jsx
+++ puppy-pals/src/main.jsx
@@ -1,14 +1,26 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
     <App />
-  </StrictMode>,
 )
 
 //What do you observe?
+/*
+ *Strict Mode tags check for react issues componets,
+ tipically we are subject to removed for better performe 
+ in our projects 
+ */
 //Can you describe what is happening in this file?
+/*Well  import function from react and react-dom/client
+ * as index.css | App from App.jsx
+  then we have the intialization of react from queryselector root
+  that help to render react|the element root tipically is found in the 
+  index.html  as div with id root.
+ */
+
 //Have you done this before?
+// yes by follow worshop and practices lectures.
